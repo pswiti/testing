2b03c38b1dbfd4ac16394cc9c980f85685d366cb
Switi <45126465+pswiti@users.noreply.github.com>
2025-06-17 15:16:22 -0700

    Update and rename demotest.txt to main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "appservice-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = var.app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    linux_fx_version = "PYTHON|3.11"
+  }
+
+  app_settings = {
+    "WEBSITES_PORT" = "8000"
+    "STARTUP_COMMAND" = "@startup.txt"
+  }
+
+  depends_on = [azurerm_app_service_plan.asp]
+}
